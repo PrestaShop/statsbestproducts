0448a6f8e6674cf7dacbe6e124f3ced0528873e7
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-08-21 14:35:15 +0200

    //Version update

--- statsbestproducts.php
+++ statsbestproducts.php
@@ -41,7 +41,7 @@ class StatsBestProducts extends ModuleGrid
 	{
 		$this->name = 'statsbestproducts';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.3';
+		$this->version = '1.4.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
