ea9117d6f472a09b03e746852bdbf732fd844153
antoin_m <michel.br.antoine@gmail.com>
2017-01-11 10:16:56 +0100

    Update max version compliancy

--- statsbestproducts.php
+++ statsbestproducts.php
@@ -111,7 +111,7 @@ class StatsBestProducts extends ModuleGrid
 
 		$this->displayName = $this->l('Best-selling products');
 		$this->description = $this->l('Adds a list of the best-selling products to the Stats dashboard.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.7.0.99');
 	}
 
 	public function install()
